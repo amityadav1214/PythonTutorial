4bc2d379be0f020d0cd1e46a501375c902d8721d
Amit Chandrakant Yadav <125544278+amityadav1214@users.noreply.github.com>
2025-07-28 10:15:32 +0530

    Assignment 1 Completion

---
+++ Assignment1.py
@@ -0,0 +1,16 @@
+# Task 1 :- Using Arithmetic Operations
+'''
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+# Arithmetic Operations
+print("Addition of two numbers is : ", a + b)
+print("Subtraction of two numbers is : ", a - b)
+print("Multiplication of two numbers is : ", a * b)
+print("Division of two numbers is : ", a / b)
+'''
+
+# Task 2 :- String Concatenation
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello " + first_name + " " + last_name + "! Welcome to the Python programming world.")
