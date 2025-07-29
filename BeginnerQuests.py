0c72082e4d697448bc44e7111c4e4f9b39a91587
Amit Chandrakant Yadav <125544278+amityadav1214@users.noreply.github.com>
2025-07-29 12:07:53 +0530

    Update BeginnerQuests.py

--- BeginnerQuests.py
+++ BeginnerQuests.py
@@ -38,16 +38,33 @@
 # print("Number of vowels in the string:", vowel_count)
 
 # Problem 4 :- Find a Maximum number in a list without using max()
-def find_max(lst):
-    if not list:
-        return None
-    max_num = lst[0]
-    for num in lst:
-        if num > max_num:
-            max_num = num
-    return max_num
+# def find_max(lst):
+#     if not list:
+#         return None
+#     max_num = lst[0]
+#     for num in lst:
+#         if num > max_num:
+#             max_num = num
+#     return max_num
+
+# # Example usage
+# input_list = list(map(int, input("Enter numbers separated by space: ").split()))
+# max_number = find_max(input_list)
+# print("Maximum number in the list:", max_number)
+
+# Problem 5 :- Print numbers from 1 to 50. For multiples of 3, print "Fizz", 
+# for multiples of 5, print "Buzz", and for multiples of both, print "FizzBuzz".
+
+def fizz_buzz():
+    for i in range(1,51):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
 
 # Example usage
-input_list = list(map(int, input("Enter numbers separated by space: ").split()))
-max_number = find_max(input_list)
-print("Maximum number in the list:", max_number)
+fizz_buzz()
